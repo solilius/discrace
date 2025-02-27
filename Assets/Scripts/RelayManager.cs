6c3d43b49db071915b7ed295b5a75f52ed7a57b4
Sol Elan <solipod8@gmail.com>
2025-02-27 21:44:46 +0200

    Use updated package

--- Assets/Scripts/RelayManager.cs
+++ Assets/Scripts/RelayManager.cs
@@ -2,7 +2,6 @@ using System.Threading.Tasks;
 using TMPro;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
-using Unity.Networking.Transport.Relay;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Relay;
@@ -12,8 +11,9 @@ using UnityEngine;
 public class RelayManager : MonoBehaviour
 {
     [SerializeField] private TMP_InputField codeInput;
-
-    private string CONNECTION_TYPE = "wss";
+    
+    private const string ConnectionType = "wss";
+    
     async void Start()
     {
         await UnityServices.InitializeAsync();
@@ -35,21 +35,10 @@ public class RelayManager : MonoBehaviour
     
     private async Task<string> StartHostWithRelay(int maxConnections = 4)
     {
-        // Create an allocation with the specified number of connections
         Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
 
-        // RelayServerData relayServerData = new RelayServerData(
-        //     allocation.RelayServer.IpV4,
-        //     (ushort)allocation.RelayServer.Port,
-        //     allocation.AllocationIdBytes,
-        //     allocation.ConnectionData,
-        //     allocation.ConnectionData,
-        //     allocation.Key,
-        //     true
-        // );
-        //
-        // NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
-        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, CONNECTION_TYPE));
+        var relayServerData = allocation.ToRelayServerData(ConnectionType);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
         string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
 
         return NetworkManager.Singleton.StartHost() ? joinCode : null;
@@ -58,17 +47,8 @@ public class RelayManager : MonoBehaviour
     private async Task<bool> StartClientWithRelay(string joinCode)
     {
         JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
-        // RelayServerData relayServerData = new RelayServerData(
-        //     joinAllocation.RelayServer.IpV4,
-        //     (ushort)joinAllocation.RelayServer.Port,
-        //     joinAllocation.AllocationIdBytes,
-        //     joinAllocation.ConnectionData,
-        //     joinAllocation.ConnectionData,
-        //     joinAllocation.Key,
-        //     true
-        // );
-        // NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
-        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, CONNECTION_TYPE));
+        var relayServerData = joinAllocation.ToRelayServerData(ConnectionType);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
 
         return !string.IsNullOrEmpty(joinCode) && NetworkManager.Singleton.StartClient();
     }
